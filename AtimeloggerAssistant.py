271a7f79bf79d8337d9deb62e9f407a04fb16848
kangfa <1470298128@qq.com>
2021-04-16 08:11:36 +0800

    对旧代码的简单说明，并着手准备重构

---
+++ AtimeloggerAssistant.py
@@ -0,0 +1,13 @@
+
+class AtimeloggerAssistant:
+    def __init__(self):
+        pass
+
+    def add_report(self):
+        pass
+
+    def simplified(self):
+        pass
+
+    def summarizing(self, interval):
+        pass
